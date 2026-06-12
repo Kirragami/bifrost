ffce821af375d3037dc9ca6d85466a4dd575c7d1
Kirragami <abhi.kirra@gmail.com>
2026-06-12 15:24:30 +0630

    fix install script to use target install path's link for editability

--- scripts/install.sh
+++ scripts/install.sh
@@ -23,10 +23,9 @@ fi
 echo "==> Setting up system venv..."
 python3 -m venv "$APP_DIR/.venv"
 "$APP_DIR/.venv/bin/pip" install --upgrade pip
-"$APP_DIR/.venv/bin/pip" install .
+"$APP_DIR/.venv/bin/pip" install -e $APP_DIR
 
 echo "==> Linking binary..."
-
 ln -sf "$APP_DIR/.venv/bin/bifrost" "$BIN_TARGET"
 
 echo "==> Installation complete! You can now run 'bifrost' from anywhere."
