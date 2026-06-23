08028228f19f7fb40734fbc841775f3c211dbd74
Kirragami <abhi.kirra@gmail.com>
2026-06-23 16:22:22 +0630

    aggressive appimage killing for stubborn OSes

--- src/bifrost/core/bifrostengine.py
+++ src/bifrost/core/bifrostengine.py
@@ -63,7 +63,9 @@ class BifrostEngine:
         if self.process and self.process.poll() is None:
             try:
                 pgid = os.getpgid(self.process.pid)
-                os.killpg(pgid, signal.SIGKILL) 
+                print(f"Killing PID: {pgid}")
+                os.killpg(pgid, signal.SIGTERM)
+                os.killpg(pgid, signal.SIGKILL)
                 self.process.wait(timeout=5)
             except (OSError, subprocess.TimeoutExpired):
                 try:
